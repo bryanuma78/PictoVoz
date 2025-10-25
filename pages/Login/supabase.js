4665e55896dd778e74ba918c8681d23211bdeafa
jwolf <bradjoey7@gmail.com>
2025-10-25 12:06:11 -0500

    actualizacion

--- pages/Login/supabase.js
+++ pages/Login/supabase.js
@@ -1,7 +1,7 @@
 // supabase.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-const SUPABASE_URL = "https://TU_PROYECTO.supabase.co"; // 🔹 cambia esto
-const SUPABASE_ANON_KEY = "TU_ANON_KEY"; // 🔹 cambia esto
+const SUPABASE_URL = "https://kicxfzxzubvpivzwlfsy.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpY3hmenh6dWJ2cGl2endsZnN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMzg2NjcsImV4cCI6MjA3NjkxNDY2N30.UGpuS698M8OhN5tMWEDFHjXtcuj6LiyL_pOYv_N8z5I";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
