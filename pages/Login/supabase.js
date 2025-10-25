41ec3d69118c41c9f0758caacbeab74e0de1f126
jwolf <bradjoey7@gmail.com>
2025-10-25 13:58:49 -0500

    actualizacion

--- pages/Login/supabase.js
+++ pages/Login/supabase.js
@@ -1,7 +1,9 @@
-// supabase.js
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
+// ✅ /pages/Login/supabase.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = "https://kicxfzxzubvpivzwlfsy.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpY3hmenh6dWJ2cGl2endsZnN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMzg2NjcsImV4cCI6MjA3NjkxNDY2N30.UGpuS698M8OhN5tMWEDFHjXtcuj6LiyL_pOYv_N8z5I";
+// 🔹 Configuración de tu proyecto Supabase
+const SUPABASE_URL = 'https://yxbxfdwotdzuryicjyxs.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4YnhmZHdvdGR6dXJ5aWNqeXhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0MDkyOTMsImV4cCI6MjA3Njk4NTI5M30.9dli0P2wfZhQYjxtA-7vThfKnbaWuBQLR41bH1Ju2JU';
 
+// 🔹 Crear cliente Supabase
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
